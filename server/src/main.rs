a953a746f766aa9af0e90627d21f6094ccfab559
Vincenzo <vincenzosorcigli@gmail.com>
2025-06-02 21:28:58 -0400

    allow all cors origins

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,5 @@
 use axum::{routing::post, Router};
-
+use tower_http::cors::{Any, CorsLayer};
 mod routes {
     pub mod og_wind;
 }
@@ -8,8 +8,16 @@ mod ffi;
 
 #[tokio::main]
 async fn main() {
+    // allow all cors origins
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     // build our application with a single route
-    let app = Router::new().route("/wind2seed", post(routes::og_wind::find_og_wind_route));
+    let app = Router::new()
+        .route("/wind2seed", post(routes::og_wind::find_og_wind_route))
+        .layer(cors);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
